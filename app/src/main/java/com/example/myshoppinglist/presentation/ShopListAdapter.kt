7531ba7b33466686f4fea96b85028971b6eaa24f
NikaNed <babenkova.nika@ya.ru>
2022-02-16 16:42:40 +0300

    Prepare xml ShopItemActivity

--- app/src/main/java/com/example/myshoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/example/myshoppinglist/presentation/ShopListAdapter.kt
@@ -1,18 +1,11 @@
 package com.example.myshoppinglist.presentation
 
-import android.content.Context
-import android.util.Log
+
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ListAdapter
-import android.widget.TextView
-import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.RecyclerView
 import com.example.myshoppinglist.R
 import com.example.myshoppinglist.domain.ShopItem
-import com.example.myshoppinglist.presentation.ShopListAdapter.*
+
 
 class ShopListAdapter: androidx.recyclerview.widget.ListAdapter<ShopItem,
         ShopItemViewHolder>(ShopItemDiffCallback()) {
